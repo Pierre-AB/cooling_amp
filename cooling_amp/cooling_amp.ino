38953dd1f99c29d59e4e5b391f77ce1f1b454d09
Pierre-Alderic Boegler <pa.boegler@gmail.com>
2021-04-16 22:53:49 +0200

    Code working with TMP36 sensor - PWM to implement

--- cooling_amp/cooling_amp.ino
+++ cooling_amp/cooling_amp.ino
@@ -1,14 +1,35 @@
-const int sensorPin = A0;
+const int firstSensorPin = A0;
+const float baselineTemp = 30.0;
 const float firstTemp = 0;
-
+const int switchFanPin = 7; //3
+const int fanPwm = 5;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(switchFanPin, OUTPUT);
+  digitalWrite(switchFanPin, LOW);
   
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int sensorVal = analogRead(firstSensorPin);
+  Serial.print("Valeur capteur : ");
+  Serial.print(sensorVal);
+
+  float voltage = (sensorVal/1024.0)*5.0;
+  Serial.print(", Volts : ");
+  Serial.print(voltage);
+  Serial.print(", degres C : ");
+  float temperature = (voltage - 0.5)*100;
+  Serial.println(temperature);
+
+
+  if(temperature > baselineTemp) {
+    digitalWrite(switchFanPin, HIGH);
+    } else {
+      digitalWrite(switchFanPin, LOW);
+      
+      }
 
 }
